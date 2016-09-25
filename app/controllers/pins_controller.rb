0a162dd03467e42879e6588a5087dae438240adf
Radhika Gulati <radhigulati@gmail.com>
2016-09-25 16:12:23 -0700

    added formatting for forms

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,53 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+  end
+
+  # GET /pins/new
+  def new
+    @pin = Pin.new
+  end
+
+  def edit
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+ 
+  def destroy
+    @pin.destroy
+    redirect_to pins_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pins
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" #creates about_path
